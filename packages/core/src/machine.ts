8de41b40a85c022e12ed4153da32d0f64e624779
Joshua Amaju <princejosh0007@gmail.com>
2021-01-29 16:15:54 +0000

    build

--- packages/core/src/machine.ts
+++ packages/core/src/machine.ts
@@ -1,14 +1,22 @@
-import {assign, createMachine, send, spawn, SpawnedActorRef} from 'xstate';
-import {choose, pure} from 'xstate/lib/actions';
-import {createActor} from './actor';
-import {Config, FaumRecord, Schema, ValidationSchema} from './types';
-import {toSchema} from './utils';
+import {
+  actions,
+  assign,
+  createMachine,
+  send,
+  spawn,
+  SpawnedActorRef,
+} from "xstate";
+import { createActor } from "./actor";
+import { Config, FaumRecord, ValidationSchema } from "./types";
+import { toSchema } from "./utils";
+
+const { choose, pure } = actions;
 
 export type Context<T, K = unknown> = {
   data?: K;
   error?: Error;
   values: FaumRecord<T>;
-  type: 'save' | 'submit';
+  type: "save" | "submit";
   validatedActors: string[];
   schema: ValidationSchema<T>;
   errors: Map<keyof T, string>;
@@ -16,38 +24,38 @@ export type Context<T, K = unknown> = {
 };
 
 export type SetType<T, K> =
-  | {name: 'data'; value: Context<T, K>['data']}
-  | {name: 'values'; value: Context<T, K>['values']}
-  | {name: 'error'; value: Context<T, K>['error']}
-  | {name: 'errors'; value: Context<T, K>['errors']}
-  | {name: 'schema'; value: ValidationSchema<T>};
+  | { name: "data"; value: Context<T, K>["data"] }
+  | { name: "values"; value: Context<T, K>["values"] }
+  | { name: "error"; value: Context<T, K>["error"] }
+  | { name: "errors"; value: Context<T, K>["errors"] }
+  | { name: "schema"; value: ValidationSchema<T> };
 
 export type Events<T, K> =
-  | {type: 'BLUR' | 'EDIT'; name: keyof T; value: T[keyof T] | null}
-  | {type: 'ACTOR_ERROR'; name: keyof T; error: string}
-  | ({type: 'SET'} & SetType<T, K>)
-  | {type: 'ACTOR_NO_ERROR'; name: string}
-  | {type: 'SAVE'; validate?: boolean}
-  | {type: 'VALIDATE'; name: keyof T}
-  | {type: 'SUBMIT'};
+  | { type: "BLUR" | "EDIT"; name: keyof T; value: T[keyof T] | null }
+  | { type: "ACTOR_ERROR"; name: keyof T; error: string }
+  | ({ type: "SET" } & SetType<T, K>)
+  | { type: "ACTOR_NO_ERROR"; name: string }
+  | { type: "SAVE"; validate?: boolean }
+  | { type: "VALIDATE"; name: keyof T }
+  | { type: "SUBMIT" };
 
 export type States<T, K = unknown> =
   | {
       value:
-        | 'editing'
-        | 'validating'
-        | 'submitting'
-        | 'validatingActors'
-        | 'saving'
-        | 'saved';
+        | "editing"
+        | "validating"
+        | "submitting"
+        | "validatingActors"
+        | "saving"
+        | "saved";
       context: Context<T, K>;
     }
   | {
-      value: 'submitted';
-      context: Context<T, K> & {data: K};
+      value: "submitted";
+      context: Context<T, K> & { data: K };
     };
 
-const allActorsValidated = ({actors, validatedActors}: any) => {
+const allActorsValidated = ({ actors, validatedActors }: any) => {
   return validatedActors.length === Object.keys(actors).length;
 };
 
@@ -60,21 +68,21 @@ export const createFormMachine = <T, K>({
 }: Config<T, K>) => {
   return createMachine<Context<T, K>, Events<T, K>, States<T, K>>(
     {
-      id: 'form',
-      initial: 'editing',
+      id: "form",
+      initial: "editing",
       context: {
-        type: 'submit',
+        type: "submit",
         errors: new Map(),
         validatedActors: [],
-        actors: {} as Context<T, K>['actors'],
-        values: {} as Context<T, K>['values'],
-        schema: schema ?? ({} as Context<T, K>['schema']),
+        actors: {} as Context<T, K>["actors"],
+        values: {} as Context<T, K>["values"],
+        schema: schema ?? ({} as Context<T, K>["schema"]),
       },
       entry: [
-        'setInitialValues',
+        "setInitialValues",
         choose([
           {
-            actions: 'spawnActors',
+            actions: "spawnActors",
             cond: () => Boolean(schema),
           },
         ]),
@@ -82,15 +90,15 @@ export const createFormMachine = <T, K>({
       on: {
         SET: {
           actions: [
-            'setValue',
+            "setValue",
             choose([
               {
-                actions: 'sendEditToActors',
-                cond: (_, {name}) => name === 'values',
+                actions: "sendEditToActors",
+                cond: (_, { name }) => name === "values",
               },
               {
-                cond: (_, {name}) => name === 'schema',
-                actions: ['assignSchema', 'spawnActors'],
+                cond: (_, { name }) => name === "schema",
+                actions: ["assignSchema", "spawnActors"],
               },
             ]),
           ],
@@ -100,21 +108,21 @@ export const createFormMachine = <T, K>({
         editing: {
           on: {
             ACTOR_ERROR: {
-              actions: 'assignActorError',
+              actions: "assignActorError",
             },
             ACTOR_NO_ERROR: {
-              actions: 'clearActorError',
+              actions: "clearActorError",
             },
             BLUR: {
-              actions: ['sendBlurToActor', 'assignValue'],
+              actions: ["sendBlurToActor", "assignValue"],
             },
             EDIT: {
-              actions: ['sendEditToActor', 'assignValue'],
+              actions: ["sendEditToActor", "assignValue"],
             },
             VALIDATE: {
-              actions: 'sendValidateToActor',
+              actions: "sendValidateToActor",
             },
-            SUBMIT: 'validatingActors',
+            SUBMIT: "validatingActors",
             // SAVE: 'beforeSave',
           },
 
@@ -135,28 +143,28 @@ export const createFormMachine = <T, K>({
           // ],
         },
         validatingActors: {
-          exit: 'clearMarkedActors',
-          entry: 'sendValidateToActors',
+          exit: "clearMarkedActors",
+          entry: "sendValidateToActors",
           always: [
             {
-              target: 'editing',
-              cond: 'allActorsValidatedAndHasErrors',
+              target: "editing",
+              cond: "allActorsValidatedAndHasErrors",
             },
             {
-              target: 'validating',
-              cond: 'allActorsValidated',
+              target: "validating",
+              cond: "allActorsValidated",
             },
           ],
           on: {
-            '*': {
+            "*": {
               actions: [
-                'markActor',
+                "markActor",
                 choose([
                   {
-                    cond: 'isErrorEvent',
-                    actions: 'assignActorError',
+                    cond: "isErrorEvent",
+                    actions: "assignActorError",
                   },
-                  {actions: 'clearActorError'},
+                  { actions: "clearActorError" },
                 ]),
               ],
             },
@@ -164,107 +172,107 @@ export const createFormMachine = <T, K>({
         },
         validating: {
           invoke: {
-            src: 'validateForm',
+            src: "validateForm",
             onDone: [
               {
-                target: 'submitting',
-                cond: ({type}) => type === 'submit',
+                target: "submitting",
+                cond: ({ type }) => type === "submit",
               },
-              {target: 'saving'},
+              { target: "saving" },
             ],
             onError: {
-              target: 'editing',
-              actions: 'assignErrors',
+              target: "editing",
+              actions: "assignErrors",
             },
           },
         },
         submitting: {
-          entry: 'clearError',
+          entry: "clearError",
           invoke: {
-            src: 'submitForm',
+            src: "submitForm",
             onDone: [
               {
-                target: 'submitted',
-                actions: 'assignData',
+                target: "submitted",
+                actions: "assignData",
                 cond: () => once,
               },
               {
-                target: 'editing',
-                actions: 'assignData',
+                target: "editing",
+                actions: "assignData",
               },
             ],
             onError: {
-              target: 'editing',
-              actions: 'assignError',
+              target: "editing",
+              actions: "assignError",
             },
           },
         },
         saving: {
           invoke: {
-            src: 'saveForm',
+            src: "saveForm",
             onDone: [
               {
-                target: 'saved',
+                target: "saved",
                 cond: () => once,
               },
-              {target: 'editing'},
+              { target: "editing" },
             ],
             onError: {
-              target: 'editing',
-              actions: 'assignError',
+              target: "editing",
+              actions: "assignError",
             },
           },
         },
         saved: {
-          type: 'final',
+          type: "final",
         },
         submitted: {
-          type: 'final',
-          data: ({data}) => data,
+          type: "final",
+          data: ({ data }) => data,
         },
       },
     },
     {
       guards: {
         allActorsValidated,
-        isErrorEvent: (_, {type}) => type === 'ACTOR_ERROR',
-        allActorsValidatedAndHasErrors: ({errors, ...ctx}) => {
+        isErrorEvent: (_, { type }) => type === "ACTOR_ERROR",
+        allActorsValidatedAndHasErrors: ({ errors, ...ctx }) => {
           return allActorsValidated(ctx) && errors.size > 0;
         },
       },
       actions: {
-        assignData: assign({data: (_, {data}: any) => data}),
-        assignError: assign({error: (_, {data}: any) => data}),
-        clearError: assign((ctx) => ({...ctx, error: undefined})),
+        assignData: assign({ data: (_, { data }: any) => data }),
+        assignError: assign({ error: (_, { data }: any) => data }),
+        clearError: assign((ctx) => ({ ...ctx, error: undefined })),
 
-        sendBlurToActor: send((_, e) => ({...e, validate: true}), {
-          to: (_, {name}: any) => name,
+        sendBlurToActor: send((_, e) => ({ ...e, validate: true }), {
+          to: (_, { name }: any) => name,
         }),
 
         sendEditToActor: send((_, e) => e, {
-          to: (_, {name}: any) => name,
+          to: (_, { name }: any) => name,
         }),
 
-        sendValidateToActor: send('VALIDATE', {
-          to: (_, {name}: any) => name,
+        sendValidateToActor: send("VALIDATE", {
+          to: (_, { name }: any) => name,
         }),
 
-        setValue: assign((ctx, {name, value}: any) => {
-          return {...ctx, [name]: value};
+        setValue: assign((ctx, { name, value }: any) => {
+          return { ...ctx, [name]: value };
         }),
 
         assignValue: assign({
-          values: ({values}, {name, value}: any) => {
-            return {...values, [name]: value};
+          values: ({ values }, { name, value }: any) => {
+            return { ...values, [name]: value };
           },
         }),
 
         assignErrors: assign({
-          errors: (_, {data}: any) => data,
+          errors: (_, { data }: any) => data,
         }),
 
         assignActorError: assign({
-          errors: ({errors}, {name, error}: any) => {
+          errors: ({ errors }, { name, error }: any) => {
             const errs = new Map(errors);
             errs.set(name, error);
             return errs;
@@ -272,51 +280,51 @@ export const createFormMachine = <T, K>({
         }),
 
         clearActorError: assign({
-          errors: ({errors}, {name}: any) => {
+          errors: ({ errors }, { name }: any) => {
             const errs = new Map(errors);
             errs.delete(name);
             return errs;
           },
         }),
 
-        sendEditToActors: pure(({actors}, {value}: any) => {
+        sendEditToActors: pure(({ actors }, { value }: any) => {
           return Object.keys(actors).map((key) => {
-            return send({type: 'EDIT', value: value?.[key]}, {to: key});
+            return send({ type: "EDIT", value: value?.[key] }, { to: key });
           });
         }),
 
-        sendValidateToActors: pure(({actors}) => {
+        sendValidateToActors: pure(({ actors }) => {
           return Object.keys(actors).map((key) => {
-            return send('VALIDATE', {to: key});
+            return send("VALIDATE", { to: key });
           });
         }),
 
         markActor: assign({
-          validatedActors: ({validatedActors}, {type, name}: any) => {
+          validatedActors: ({ validatedActors }, { type, name }: any) => {
             return [...validatedActors, name];
           },
         }),
 
-        clearMarkedActors: assign((ctx) => ({...ctx, validatedActors: []})),
+        clearMarkedActors: assign((ctx) => ({ ...ctx, validatedActors: [] })),
 
-        assignSchema: assign((ctx, {schema}: any) => {
-          return {...ctx, schema};
+        assignSchema: assign((ctx, { schema }: any) => {
+          return { ...ctx, schema };
         }),
 
-        setInitialValues: assign(({schema, ...ctx}) => {
-          const values = {} as Context<T, K>['values'];
+        setInitialValues: assign(({ schema, ...ctx }) => {
+          const values = {} as Context<T, K>["values"];
 
           Object.keys(schema).forEach((k) => {
             const key = k as keyof T;
-            const {initialValue} = toSchema(schema[key]);
+            const { initialValue } = toSchema(schema[key]);
             values[key] = initialValue;
           });
 
-          return {...ctx, values};
+          return { ...ctx, values };
         }),
 
-        spawnActors: assign(({schema, ...ctx}) => {
-          const actors = {} as Context<T, K>['actors'];
+        spawnActors: assign(({ schema, ...ctx }) => {
+          const actors = {} as Context<T, K>["actors"];
 
           Object.keys(schema).forEach((key) => {
             const _key = key as keyof T;
@@ -324,17 +332,17 @@ export const createFormMachine = <T, K>({
             actors[_key] = spawn(createActor(key, value), key);
           });
 
-          return {...ctx, actors};
+          return { ...ctx, actors };
         }),
       },
       services: {
-        submitForm({values}) {
+        submitForm({ values }) {
           return onSubmit(values);
         },
-        async saveForm({values}) {
+        async saveForm({ values }) {
           return onSave?.(values);
         },
-        validateForm({values}) {
+        validateForm({ values }) {
           let errors = validate?.(values);
 
           if (errors && Object.values(errors).length > 0) {
