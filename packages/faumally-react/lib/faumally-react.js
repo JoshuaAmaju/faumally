68408043ae3116309710b8cddf26832c79313791
Joshua Amaju <princejosh0007@gmail.com>
2021-02-01 10:33:40 +0100

    chore(faumally-react): setup

--- packages/faumally-react/lib/faumally-react.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = faumallyReact;
-
-function faumallyReact() {
-    // TODO
-}
---
+++ packages/faumally-react/rollup.config.js
@@ -0,0 +1,39 @@
+import {terser} from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+
+const external = ['xstate', 'react'];
+
+export default [
+  {
+    external,
+    input: 'src/index.ts',
+    output: {
+      dir: 'es',
+      format: 'esm',
+    },
+    plugins: [nodeResolve (), typescript (), terser ()],
+  },
+  {
+    external,
+    input: 'src/index.ts',
+    output: {
+      format: 'iife',
+      name: 'FaumallyReact',
+      file: 'dist/faumally-react.js',
+      globals: {
+        xstate: 'XState',
+        react: 'React',
+      },
+    },
+    plugins: [
+      nodeResolve (),
+      typescript ({
+        tsconfigOverride: {
+          declarations: false,
+        },
+      }),
+      terser (),
+    ],
+  },
+];
