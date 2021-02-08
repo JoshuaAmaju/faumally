c068e8abbc908be9b347ec163bd7039edb8a9508
Joshua Amaju <princejosh0007@gmail.com>
2021-02-08 09:48:24 +0100

    chore: add temp code

---
+++ packages/faumally-react/src/index.ts
@@ -0,0 +1,28 @@
+// export default function useForm<T, K = unknown>(config: Config<T, K>) {
+//   const {
+//     current: {service, ...rest},
+//   } = useRef(_useForm(config));
+
+//   const [state] = useService(service) as any;
+
+//   const {
+//     context: {data, values, errors, error},
+//   } = state;
+
+//   const hasErrors = errors.size > 0;
+
+//   const isSubmitting = state.matches('submitting');
+
+//   const hasError = (name: keyof T) => errors.has(name);
+
+//   return {
+//     ...rest,
+//     data,
+//     error,
+//     values,
+//     errors,
+//     hasError,
+//     hasErrors,
+//     isSubmitting,
+//   };
+// }
