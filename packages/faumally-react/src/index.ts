f038784bb7ea656f571cab7591a67b8911d91de6
Joshua Amaju <princejosh0007@gmail.com>
2021-02-17 14:04:07 +0100

    chore: build

--- packages/faumally-react/src/index.ts
+++ packages/faumally-react/src/index.ts
@@ -1,21 +1,21 @@
-import { useRef } from "react";
-import { useService } from "@xstate/react";
-import { useFaum } from "faumally";
+import {useRef} from 'react';
+import {useService} from '@xstate/react';
+import {useFaum, Config} from 'faumally';
 
 export default function useFaumally<T, K = unknown>(config: Config<T, K>) {
   const {
-    current: { service, ...rest },
+    current: {service, ...rest},
   } = useRef(useFaum(config));
 
-  const [state] = useService(service) as any;
+  const [state] = useService(service);
 
   const {
-    context: { data, values, errors, error },
+    context: {data, values, errors, error},
   } = state;
 
   const hasErrors = errors.size > 0;
 
-  const isSubmitting = state.matches("submitting");
+  const isSubmitting = state.matches('submitting');
 
   const hasError = (name: keyof T) => errors.has(name);
 
