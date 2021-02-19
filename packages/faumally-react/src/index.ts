3eb95480affb3b68b031bbb3c26eddf95c3be649
Joshua Amaju <princejosh0007@gmail.com>
2021-02-19 11:17:56 +0100

    chore: replace internals

--- packages/faumally-react/src/index.ts
+++ packages/faumally-react/src/index.ts
@@ -1,56 +1,21 @@
 import {useRef, useMemo} from 'react';
 import {useService} from '@xstate/react';
-import {useFaum, Config} from 'faumally';
+import {Faumally, Config, _getContext} from 'faumally';
 
 export default function useFaumally<T, K = unknown>(config: Config<T, K>) {
   const {
     current: {service, generateHandlers, ...rest},
-  } = useRef(useFaum(config));
+  } = useRef(Faumally(config));
 
   const [state] = useService(service);
 
-  const {
-    context: {data, values, errors, error, schema},
-  } = state;
+  const context = _getContext(state);
 
-  const hasErrors = errors.size > 0;
-
-  const isSaving = state.matches('saving');
-
-  const isSubmitting = state.matches('submitting');
-
-  const hasError = (name: keyof T) => errors.has(name);
-
-  const handlers = useMemo(generateHandlers, [schema]);
-
-  const attemptedSaveOrSubmit =
-    state.matches('editing') &&
-    (state.history?.matches('validatingActors') ||
-      state.history?.matches('validating'));
-
-  const saved =
-    state.matches('editing') && state.history?.matches('saving') ? true : false;
-
-  const submitted =
-    state.matches('submitted') ||
-    (state.matches('editing') && state.history?.matches('submitting'))
-      ? true
-      : false;
+  const handlers = useMemo(generateHandlers, [config.schema]);
 
   return {
     ...rest,
-    data,
-    error,
-    saved,
-    values,
-    errors,
-    service,
-    hasError,
+    ...context,
     handlers,
-    isSaving,
-    submitted,
-    hasErrors,
-    isSubmitting,
-    attemptedSaveOrSubmit,
   };
 }
