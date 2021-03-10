70c779b6606d417a3e0c649451e1b775ad97efa5
Joshua Amaju <princejosh0007@gmail.com>
2021-03-10 23:33:37 +0100

    temp build

--- packages/faumally-react/src/index.ts
+++ packages/faumally-react/src/index.ts
@@ -1,21 +1,23 @@
-import { useRef, useMemo } from "react";
-import { useService } from "@xstate/react";
-import { Faumally, Config, _getContext } from "faumally";
+// import { useRef, useMemo } from "react";
+// import { useService } from "@xstate/react";
+// import { Faumally, Config, _getContext } from "faumally";
 
-export default function useFaumally<T, K = unknown>(config: Config<T, K>) {
-  const { service, generateHandlers, ...rest } = useRef(
-    Faumally<T, K>(config)
-  ).current;
+// export default function useFaumally<T, K = unknown>(config: Config<T, K>) {
+//   const { service, generateHandlers, ...rest } = useRef(
+//     Faumally<T, K>(config)
+//   ).current;
 
-  const [state] = useService(service);
+//   const [state] = useService(service);
 
-  const context = useMemo(() => _getContext(state), [state]);
+//   const context = useMemo(() => _getContext(state), [state]);
 
-  const handlers = useMemo(generateHandlers, [config.schema]);
+//   const handlers = useMemo(generateHandlers, [config.schema]);
 
-  return {
-    ...rest,
-    ...context,
-    handlers,
-  };
-}
+//   return {
+//     ...rest,
+//     ...context,
+//     handlers,
+//   };
+// }
+
+export const name = "Faumally";
